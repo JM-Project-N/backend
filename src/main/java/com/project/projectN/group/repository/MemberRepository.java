823a4ddcdd269f8d735da2fc132f8c8b99360bf1
Tizesin <tlsalswns0o@gmail.com>
2025-04-11 00:54:41 +0900

    projectN making dir

---
+++ src/main/java/com/project/projectN/group/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.project.projectN.group.repository;
+
+import com.project.projectN.group.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+
+}
